89c3f6eb98e75723b50ddc76fef293a9c274e09e
bivach <abivachi@gmail.com>
2017-04-20 16:01:19 -0300

    add validation to register and save users

--- register.php
+++ register.php
@@ -2,18 +2,41 @@
 include("validacion.php");
 $usernameDefault ="";
 $emaildefault = "";
+$usernameError ="usuario";
+$passwordError ="contraseña";
+$nameDefault="";
+$nameError = "nombre";
 if ($_POST) {
   $errors = validateRegister($_POST);
   if (count($errors) == 0) {
+    //ACA HABRIA QUE GUARDAR AL USUARIO EN CASO DE QUE NO HUBIERA HABIADO ERROR EN LA REGISTRACION.
+
+    $usuario = crearUsuario($_POST);
+
+			//Guardar el usuario
+			guardarUsuario($usuario);
+
     header("location:login.php");exit;
-  }
-  if (!isset($errores["usuario"])) {
-    $usernameDefault = $_POST["usuario"];
   }
   if (!isset($errores["mail"])) {
     $emaildefault = $_POST["mail"];
   }
-  var_dump($errors);
+
+  if (!isset($errors["usuario"])) {
+    $usernameDefault = $_POST["usuario"];
+  }else {
+    $usernameError = $errors["usuario"];
+  }
+
+  if (!isset($errors["name"])) {
+    $nameDefault = $_POST["name"];
+  }else {
+    $nameError = $errors["name"];
+  }
+  if (isset($errors["password"])) {
+    $passwordError = $errors["password"];
+  }
+
 
 }
 
@@ -25,10 +48,52 @@ include ("header.php");
   <div class="form">
 
     <form class="login-form" method="post" action="register.php">
-      <input type="text" name= "usuario" placeholder="usuario" value="<?=$usernameDefault?>"/>
-      <input type="password" name = "password" placeholder="contraseña"/>
-      <input type="password" name = "cpassword" placeholder="comfirmar contraseña"/>
-      <input type="email" name= "mail" placeholder="cuenta de email" value="<?=$emaildefault?>"/>
+
+      <?php
+          if (isset($errors["name"])) {?>
+            <input id= 'error' type="text" name = "name" placeholder="<?=$nameError?>" value="<?=$nameDefault?>"/>
+            <?php
+          }else {?>
+              <input type="text" name = "name" placeholder="<?=$nameError?>" value="<?=$nameDefault?>"/>
+            <?php
+          }
+       ?>
+
+      <?php
+          if (isset($errors["usuario"])) {?>
+            <input id= 'error' type="text" name = "usuario" placeholder="<?=$usernameError?>" value="<?=$usernameDefault?>"/>
+            <?php
+          }else {?>
+              <input type="usuario" name = "usuario" placeholder="<?=$usernameError?>" value="<?=$usernameDefault?>"/>
+            <?php
+          }
+       ?>
+
+       <?php
+           if (isset($errors["password"]) || isset($errors["cpassword"])) {?>
+             <input id= 'error' type="password" name = "password" placeholder="<?=$passwordError?>"/>
+             <input id= 'error' type="password" name = "cpassword" placeholder="comfirmar contraseña"/>
+             <?php
+           }else {?>
+               <input type="password" name = "password" placeholder="<?=$passwordError?>"/>
+               <input type="password" name = "cpassword" placeholder="comfirmar contraseña"/>
+             <?php
+           }
+        ?>
+
+        <?php
+            if (isset($errors["mail"])) {?>
+
+                <input id= 'error' type="email" name= "mail" placeholder="Email inválido" value="<?=$emaildefault?>"/>
+
+              <?php
+            }else {?>
+
+                <input type="email" name= "mail" placeholder="cuenta de email" value="<?=$emaildefault?>"/>
+              <?php
+            }
+         ?>
+
       <input type='submit' id = "submit" name='Submit' value='Crear Cuenta' />
       <p class="message2">Ya estas registrado? <a href="login.php">Inicia Sesión</a></p>
     </form>
--- validacion.php
+++ validacion.php
@@ -9,35 +9,40 @@ function validateLogin($datos) {
 
 	$pass = trim($_POST["password"]);
 
-	if ($pass === "" || strlen ($pass) <4) {
+	if (strlen ($pass) <4) {
 		$errores["password"] = "Contraseña incorrecta";
 	}
 
 	return $errores;
 }
 
+
 function validateRegister($datos) {
 	$errores = [];
 
-	if (trim($datos["usuario"]) === "") {
-		$errores["usuario"] = "El usuario no puede estar vacio";
+	if (trim($datos["name"]) === "") {
+		$errores["name"] = "Nombre incorrecto";
+	}
+
+	if (trim($datos["usuario"]) === "" || strlen(trim($datos["usuario"])) <4) {
+		$errores["usuario"] = "Usuario incorrecto";
 	}
 
 	$email = trim($datos["mail"]);
 
 	if ($email === "") {
-		$errores["mail"] = "Completa el campo email";
+		$errores["mail"] = "Email inválido";
 	} else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-		$errores["mail"] = "Mal formato de email";
+		$errores["mail"] = "Email inválido";
 	}
 
 	$pass = trim($_POST["password"]);
 	$cpass = trim($_POST["cpassword"]);
-	if ($pass === "") {
-		$errores["password"] = "Llena la contraseña";
+	if (strlen ($pass) <4) {
+		$errores["password"] = "Contraseña incorrecta";
 	}
-	if ($cpass === "") {
-		$errores["cpassword"] = "Llena la contraseña";
+	if (strlen ($cpass) <4) {
+		$errores["cpassword"] = "Contraseña incorrecta";
 	}
 	if ($pass != "" && $cpass != "" && $pass != $cpass) {
 		$errores["password"] = "Las contraseñas no coinciden";
@@ -45,3 +50,91 @@ function validateRegister($datos) {
 
 	return $errores;
 }
+
+
+function crearUsuario($datos) {
+		$usuario = [
+			"name" => $datos["name"],
+			"usuario" => $datos["usuario"],
+			"mail" => $datos["mail"],
+			"password" => password_hash($datos["password"], PASSWORD_DEFAULT),
+		];
+
+		$usuario["id"] = traerNuevoId();
+
+		return $usuario;
+	}
+
+	function traerNuevoId() {
+		$archivo = file_get_contents("usuarios.json");
+
+		// Si el archivo estaba vacio devuelvo 1
+		if ($archivo == "") {
+			return 1;
+		}
+
+		// Divido mi archivo por enters
+		$usuarios = explode(PHP_EOL, $archivo);
+
+		// Borro la linea del enter vacio
+		array_pop($usuarios);
+
+		// Obtengo el ultimo usuario
+		$ultimoUsuario = $usuarios[count($usuarios) - 1];
+
+		// Transformo mi ultimo usuario en formoto array
+		$ultimoUsuario = json_decode($ultimoUsuario,true);
+
+		// Devuelvo ese id + 1
+		return $ultimoUsuario["id"] + 1;
+	}
+
+
+	function guardarUsuario($usuario) {
+		$json = json_encode($usuario);
+
+		file_put_contents("usuarios.json", $json . PHP_EOL, FILE_APPEND);
+	}
+
+
+	function dameUnoPorMail($mail) {
+		$usuarios = dameTodos();
+
+		foreach ($usuarios as $usuario) {
+			if ($usuario["mail"] == $mail) {
+				return $usuario;
+			}
+		}
+
+		return NULL;
+	}
+
+
+	function dameTodos() {
+		$archivo = file_get_contents("usuarios.json");
+
+		// Lo separo linea por linea
+		$usuariosJSON = explode(PHP_EOL, $archivo);
+
+		// Borro el enter vacio
+		array_pop($usuariosJSON);
+
+		$usuariosFinal = [];
+		foreach ($usuariosJSON as $usuarioJSON) {
+			$usuariosFinal[] = json_decode($usuarioJSON,true);
+		}
+
+		return $usuariosFinal;
+	}
+
+	function dameUnoPorId($id) {
+		$usuarios = dameTodos();
+
+		foreach ($usuarios as $usuario) {
+			if ($usuario["id"] == $id) {
+				return $usuario;
+			}
+		}
+
+		return NULL;
+	}
